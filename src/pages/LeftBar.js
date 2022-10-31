b3db2edd9df5a243587758d1f80a5096e96d7362
oljen <94927686+oljen@users.noreply.github.com>
2022-10-31 06:46:38 +0000

    Add files via upload

---
+++ src/pages/LeftBar.js
@@ -0,0 +1,101 @@
+import React, { useState } from "react";
+import { ChevronDownIcon, ChevronRightIcon } from "@heroicons/react/solid";
+
+const GitLogo = require("../assets/logos/GitLogo.png");
+const LinkedinLogo = require("../assets/logos/LinkedinLogo.png");
+const MailLogo = require("../assets/logos/MailLogo.png");
+
+const JSIcon = require("../assets/icons/JSIcon.png");
+const TSIcon = require("../assets/icons/TSIcon.png");
+
+const LeftBar = () => {
+  const [showWebList, SetShowWebList] = useState(true);
+  const [showProjectsList, SetShowProjectsList] = useState(true);
+
+  const WebList = () => (
+    <div>
+      <a href="/">
+        <div className="ml-12 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] w-60">
+          <img
+            src={JSIcon}
+            alt="JS Icon"
+            className="w-7 mr-1  ml-5 text-yellow_vs"
+          />
+          <p>First Project</p>
+        </div>
+      </a>
+      <a href="https://www.youtube.com/">
+        <div className="ml-12 flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] w-60">
+          <img
+            src={TSIcon}
+            alt="TS Icon"
+            className="w-7 mr-1  ml-5 text-yellow_vs"
+          />
+          <p>Second Project</p>
+        </div>
+      </a>
+      <a href="/">
+        <div className="flex whitespace-nowrap items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] focus:bg-slate-300 ml-12 w-60">
+          <img
+            src={JSIcon}
+            alt="JS Icon"
+            className="w-7 mr-1  ml-5 text-yellow_vs"
+          />
+          <p>Third Project</p>
+        </div>
+      </a>
+      <a href="/">
+        <div className="flex whitespace-nowrap items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] ml-12 w-60">
+          <img
+            src={JSIcon}
+            alt="JS Icon"
+            className="w-7 mr-1  ml-5 text-yellow_vs"
+          />
+          <p>Fourth Project</p>
+        </div>
+      </a>
+    </div>
+  );
+
+  return (
+    <div className="text-[#a2aabc] text-lg mt-5 flex bg-[#262526] h-full">
+      <div>
+        <div
+          className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] font-bold w-72"
+          onClick={() => SetShowProjectsList(!showProjectsList)}
+        >
+          {showProjectsList ? (
+            <ChevronDownIcon className="w-7 mr-1" />
+          ) : (
+            <ChevronRightIcon className="w-7 mr-1" />
+          )}
+          <p>Projects</p>
+        </div>
+        {showProjectsList ? (
+          <>
+            <div
+              className="flex items-center hover:cursor-pointer hover:bg-opacity-80 hover:bg-[#2b2a2a] font-bold w-72"
+              onClick={() => SetShowWebList(!showWebList)}
+            >
+              {showWebList ? (
+                <ChevronDownIcon className="w-7 mr-1  ml-5" />
+              ) : (
+                <ChevronRightIcon className="w-7 mr-1  ml-5" />
+              )}
+
+              <p>Web</p>
+            </div>
+            {showWebList ? <WebList /> : null}
+          </>
+        ) : null}
+        <div className="absolute inset-x-0 bottom-10 ">
+          <div className="flex px-6 space-x-7">
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LeftBar;
